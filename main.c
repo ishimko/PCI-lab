10d97a201875b81b4706e09e4ddb24c282a3d88c
Ivan <vana.shimko@gmail.com>
2015-11-11 16:15:05 +0300

    ProcessDevice

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "pci.h"
 #include <stdlib.h>
 
+
+
 char *GetVendorName(int vendorID)
 {
 	int i;
@@ -23,6 +25,39 @@ char *GetDeviceName(int vendorID, int deviceID)
 	return NULL;
 }
 
+unsigned
+
+void ProcessDevice(unsigned short bus, unsigned short device, unsigned short function)
+{
+	unsigned long command = 0, answer = 0;
+	command = (1 << 31) | (bus << 16) | (device << 11) | (function << 8);
+	outl(command, 0x0CF8);
+	answer = inl(0x0CFC);
+
+	unsigned short deviceID = answer >> 16;
+	unsigned short vendorID = answer & 65535;
+	if (answer != 0xffffffff)
+	{
+		printf("%u.%u.%u\n", bus, device, function);
+		
+		char *vendorName = GetVendorName(vendorID);
+		char *deviceName = GetDeviceName(vendorID, deviceID);
+		
+		printf("Vendor ID: %x ", vendorID);
+		if (vendorName)
+			printf("%s\n", GetVendorName(vendorID));
+		else
+			puts("Vendor name not found.");
+			
+		printf("Device ID: %x ", deviceID);
+		if (deviceName)
+			printf("%s\n\n", GetDeviceName(vendorID, deviceID));
+		else
+			puts("Device name not found.");
+		
+	}
+	
+}
 
 int main(int argc, char **argv)
 {
@@ -32,39 +67,15 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	
-	unsigned short busID, deviceID, functionID;
-	unsigned long command = 0, answer = 0;
+	unsigned short bus, device, function;
 	
-	for (busID = 0; busID < 256; busID++)
+	for (bus = 0; bus < 256; bus++)
 	{
-		for (deviceID = 0; deviceID < 32; deviceID++)
+		for (device = 0; device < 32; device++)
 		{
-			for (functionID = 0; functionID < 8; functionID++)
+			for (function = 0; function < 8; function++)
 			{
-				command = (1 << 31) | (busID << 16) | (deviceID << 11) | (functionID << 8);
-				outl(command, 0x0CF8);
-				answer = inl(0x0CFC);
-
-				unsigned short deviceID = answer >> 16;
-				unsigned short vendorID = answer & 65535;
-				if (answer != 0xffffffff)
-				{
-					printf("%#x.%#x.%#x\n", busID, deviceID, functionID);
-					
-					char *vendorName = GetVendorName(vendorID);
-					char *deviceName = GetDeviceName(vendorID, deviceID);
-					
-					if (vendorName)
-						printf("%s\n", GetVendorName(vendorID));
-					else
-						puts("Vendor name not found.");
-						
-					if (deviceName)
-						printf("%s\n\n", GetDeviceName(vendorID, deviceID));
-					else
-						puts("Device name not found.");
-					
-				}
+				ProcessDevice(bus, device, function);
 			}
 			
 		}
